6d904a1245a982785ae2780a02b1f7b3dba8d52e
JohanFlotten <johanflotten@gmail.com>
2024-08-28 12:07:39 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ export function Header () {
         <nav className='header'>
           <Link to="/"><img src="/pokeapi.png" alt="" className="logo" /></Link>
           <ul className='list'>
-            <li id="home" className='buttons'><Link to="/">Home</Link></li>
-            <li id="1-3-gen" className='buttons'><Link to="/generations-1-3">1-3 Gen</Link></li>
-            <li id="4-6-gen" className='buttons'><Link to="/generations-4-6">4-6 Gen</Link></li>
-            <li id="7-9-gen" className='buttons'><Link to="/generations-7-9">7-9 Gen</Link></li>
+            <li id="home" ><Link to="/" className='buttons'>Home</Link></li>
+            <li id="1-3-gen" ><Link to="/generations-1-3" className='buttons'>1-3 Gen</Link></li>
+            <li id="4-6-gen" ><Link to="/generations-4-6" className='buttons'>4-6 Gen</Link></li>
+            <li id="7-9-gen" ><Link to="/generations-7-9" className='buttons'>7-9 Gen</Link></li>
           </ul>
         </nav>
       </header>
